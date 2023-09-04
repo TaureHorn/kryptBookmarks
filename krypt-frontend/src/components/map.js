8b972edf0326bf11116e673e4e6045ea16abf133
silence <abaird0893@gmail.com>
2023-09-04 17:29:24 +0100

    Added editor page to dynamically add/remove bookmark entries/categories

--- krypt-frontend/src/components/map.js
+++ krypt-frontend/src/components/map.js
@@ -1,98 +1,60 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 
-// TODO map through bookmars data WITHOUT handing off to another component
-// add ability to add and remove whole bookmark categories this time
 
 export default function Map(props) {
-  const bookmarks = props.bookmarks.data;
-  const [editMode, changeEditMode] = useState(false);
-  const [dataMap, updateDataMap] = useState("");
+  const navigate = useNavigate();
+  const dataMap = bookmarksMapper(props.bookmarks);
 
-  useEffect(() => {
-    updateDataMap(bookmarksMapper(bookmarks, editMode));
-  }, [editMode]);
-
-  console.log("bookmarks", bookmarks);
-  function bookmarksMapper(bookmarks, editMode) {
-    return editMode ? (
+  function bookmarksMapper(bookmarks) {
+    return (
       <>
-        <button onClick={() => addCategory()}>+</button>
-        {bookmarks.map((header, index) => {
-          return (
-            <div id={`${header[0]}-section${index}`} key={uuidv4()}>
-              <button onClick={() => deleteCategory(header[0])}>X</button>
-              <span className="header">{header[0]}</span>
-              <button onClick={() => addEntry()}>+</button>
-              {header[1].map((entry, index) => {
-                return (
-                  <div id={`${header[0]}-link${index}}`} key={uuidv4()}>
-                    <button onClick={() => deleteEntry(index, entry.name)}>
-                      {entry.name}
-                    </button>
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
-        <button onClick={() => toggleEditMode()}>save edits</button>
-      </>
-    ) : (
-      <>
-        {bookmarks.map((header, index) => {
-          return (
-            <div id={`${header[0]}-section${index}`} key={uuidv4()}>
-              <span className="header">{header[0]}</span>
-              {header[1].map((entry, index) => {
-                return (
-                  <div id={`${header[0]}-link${index}`} key={uuidv4()}>
-                    <a href={entry.url} target="_blank" rel="noreferrer">
-                      {entry.name}
-                    </a>
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
-        <button onClick={() => toggleEditMode()}>enable edit mode</button>
+        <div className="inline">
+          {bookmarks.map((header, index) => {
+            return (
+              <div
+                id={`${header[0]}-section${index}`}
+                key={uuidv4()}
+                className="border map padding"
+              >
+                <span className="header">{header[0]}</span>
+                {header[1].map((entry, index) => {
+                  return (
+                    <div id={`${header[0]}-link${index}`} key={uuidv4()}>
+                      <a href={entry.url} target="_blank" rel="noreferrer">
+                        {entry.name}
+                      </a>
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+        <div id="editorButtons">
+          <p>Or, start typing to search...</p>
+        </div>
       </>
     );
   }
-  function addCategory() {
-    // TODO add category addition functionality
-    console.log(`Triggered to add a category`);
-  }
-  function deleteCategory(section) {
-    // TODO add category deletion functionality
-    console.log(`Triggered to delete category ${section}`);
-  }
-  function addEntry(section) {
-    // TODO add entry addtion functionality
-    console.log(`Triggered to add an entry to ${section}`);
-  }
-  function deleteEntry(index, name) {
-    // TODO add entry deletion functionality
-    console.log(`Selected ${index}: entry ${name} is set for deletion`);
-  }
-  function toggleEditMode() {
-    if (editMode === false) {
-      changeEditMode(true);
-    } else if (editMode === true) {
-      // TODO add functionality to push current state of bookmarks up component chain and save new ones to localstorage
-      changeEditMode(false);
-    }
-  }
+
+  /////////////////////////// EVENT LISTENER ///////////////////////////
+  useEffect(() => {
+    const characters =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    const keyPressChecker = (event: KeyboardEvent) => {
+      if (characters.includes(event.key)) {
+        props.toggleSearch(true);
+      }
+    };
+    document.addEventListener("keydown", keyPressChecker);
+    return () => {
+      document.removeEventListener("keydown", keyPressChecker);
+    };
+  });
   return (
     <>
-      <h2>
-        {editMode ? (
-          <>edit mode enabled: click save button to save and exit</>
-        ) : (
-          <></>
-        )}
-      </h2>
       <div className="center">{dataMap}</div>
     </>
   );
